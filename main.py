73e5bb911e34a49509c8c7f6e4ceca43947b7dce
Francisco Tapia Trabajo <ftapia@mph.cl>
2020-04-01 13:08:01 -0300

    Agregada Funcion SPDIGITAL

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from builtins import print
 import requests
 from bs4 import BeautifulSoup
 
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
+                  'Chrome/80.0.3987.149 Safari/537.36'
+}
+
 
 def print_title(string):
     print('TITULO: {}'.format(string))
@@ -16,55 +21,58 @@ def print_price_tarjeta(string):
     print('PRECIO Tarjeta: {}'.format(string))
 
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
-                  'Chrome/80.0.3987.149 Safari/537.36'
-}
+def get_spdigital(product_id):
+    url = 'https://www.spdigital.cl/products/view/' + product_id
+
+    page = requests.get(url, headers=headers)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    title = soup.find(id='_name')
+    title_value = title.get_text().strip()
+
+    div_price_efectivo = soup.find_all('div', {
+        'class': 'product-view-cash-price-div'
+    })
+
+    price_efectivo = None
+
+    for element in div_price_efectivo:
+        price_efectivo = element.find('span', {
+            'class': 'product-view-cash-price-value text-webstore'
+        })
+
+    div_price_tarjeta = soup.find_all('div', {
+        'class': 'product-view-other-method-price-div'
+    })
+
+    price_tarjeta = None
+
+    for element in div_price_tarjeta:
+        price_tarjeta = element.find('span', {
+            'class': 'product-view-cash-price-value text-webstore'
+        })
+
+    # Muestra información en pantalla
+    print_title(title_value)
+    if price_efectivo is not None:
+        price_efectivo_value = price_efectivo.get_text().strip()
+        print_price_efectivo(price_efectivo_value)
+
+    if price_tarjeta is not None:
+        price_tarjeta_value = price_tarjeta.get_text().strip()
+        print_price_tarjeta(price_tarjeta_value)
+
+    print('--------------')
+
 
 # Caso SPDIGITAL
 
-URL = 'https://www.spdigital.cl/products/view/68271'
+product_id = '68271'
+get_spdigital(product_id)
 
-page = requests.get(URL, headers=headers)
-soup = BeautifulSoup(page.content, 'html.parser')
+product_id = '66887'
+get_spdigital(product_id)
 
-title = soup.find(id='_name')
-title_value = title.get_text().strip()
-
-div_price_efectivo = soup.find_all('div', {
-    'class': 'product-view-cash-price-div'
-})
-
-price_efectivo = None
-
-for element in div_price_efectivo:
-    price_efectivo = element.find('span', {
-        'class': 'product-view-cash-price-value text-webstore'
-    })
-
-
-div_price_tarjeta = soup.find_all('div', {
-    'class': 'product-view-other-method-price-div'
-})
-
-price_tarjeta = None
-
-for element in div_price_tarjeta:
-    price_tarjeta = element.find('span', {
-        'class': 'product-view-cash-price-value text-webstore'
-    })
-
-# Muestra información en pantalla
-print_title(title_value)
-if price_efectivo is not None:
-    price_efectivo_value = price_efectivo.get_text().strip()
-    print_price_efectivo(price_efectivo_value)
-
-if price_tarjeta is not None:
-    price_tarjeta_value = price_tarjeta.get_text().strip()
-    print_price_tarjeta(price_tarjeta_value)
-
-print('--------------')
 
 # Caso Sistemax
 
